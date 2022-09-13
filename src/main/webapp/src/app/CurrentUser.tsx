a5023c714bdcb3e6857a63fc41b63970e77199b7
Stephan Pelikan <stephan.pelikan@phactum.at>
2022-09-13 14:04:05 +0200

    Fixes

--- src/main/webapp/src/app/CurrentUser.tsx
+++ src/main/webapp/src/app/CurrentUser.tsx
@@ -1,21 +1,33 @@
 import { PropsWithChildren } from 'react';
 import { useAppContext } from '../AppContext';
+import { User } from '../client/gui';
 
 interface Props {};
 
 let promise = undefined;
+let inprogress = false;
 
 const CurrentUser = ({ children }: PropsWithChildren<Props>) => {
   
-  const { state, fetchCurrentUser } = useAppContext();
+  const { fetchCurrentUser } = useAppContext();
   
   if (promise === undefined) {
-    // return a promise to trigger <Suspend> element
-    promise = new Promise((resolve, reject) => {
-      fetchCurrentUser(resolve, reject);
+    inprogress = true;
+    promise = new Promise((resolveUser, rejectUser) => {
+      new Promise((resolveLoading, rejectLoading) => {
+          fetchCurrentUser(resolveLoading, rejectLoading);
+        })
+        .then((value: User | null) => {
+          inprogress = false;
+          resolveUser(value);
+        }).catch((error: any) => {
+          inprogress = false;
+          rejectUser(error);
+        });
     });
   }
-  if (state.currentUser == null) {
+  // return a promise to trigger <Suspend> element
+  if (inprogress) {
     throw promise;
   }
   return (<>{children}</>);
