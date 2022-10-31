3390a0d1b270fd582781dee578249f934ed535e0
Stephan Pelikan <stephan.pelikan@phactum.at>
2022-10-31 18:52:23 +0100

    Use auto-configuration for DB to ensure it runs before Liquibase

--- src/main/java/at/elmo/config/db/DbConfiguration.java
+++ src/main/java/at/elmo/config/db/DbConfiguration.java
@@ -1,14 +1,18 @@
 package at.elmo.config.db;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.util.StringUtils;
 
 import javax.sql.DataSource;
@@ -18,9 +22,10 @@ import javax.sql.DataSource;
  * @ConditionalOnMissingBean(type = "io.r2dbc.spi.ConnectionFactory")
  * which prevents running jdbc and r2dbc in parallel
  */
-@Configuration
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+@AutoConfigureBefore(LiquibaseAutoConfiguration.class)
 public class DbConfiguration extends DataSourceAutoConfiguration {
-
+    
     /**
      * Hikari DataSource configuration.
      * 
@@ -37,7 +42,7 @@ public class DbConfiguration extends DataSourceAutoConfiguration {
             name = "spring.datasource.type",
             havingValue = "at.elmo.config.db.NotificationsAwareHikariDataSource",
             matchIfMissing = true)
-    static class NotificationsAware {
+    public static class NotificationsAware {
 
         @ConditionalOnProperty(
                 prefix = "spring.jpa",
@@ -52,7 +57,7 @@ public class DbConfiguration extends DataSourceAutoConfiguration {
 
         @Bean
         @ConfigurationProperties(prefix = "spring.datasource.hikari")
-        HikariDataSource dataSource(
+        public HikariDataSource dataSource(
                 final PostgreSQLNotifications notifications,
                 final DataSourceProperties properties) {
             
