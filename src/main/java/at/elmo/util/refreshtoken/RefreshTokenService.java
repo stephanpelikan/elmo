6fb1452b6a04c74ae8f3bd35b1274653cae03e49
Stephan Pelikan <stephan.pelikan@phactum.at>
2023-01-26 15:20:11 +0100

    Fix missing TX

--- src/main/java/at/elmo/util/refreshtoken/RefreshTokenService.java
+++ src/main/java/at/elmo/util/refreshtoken/RefreshTokenService.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -103,6 +104,7 @@ public class RefreshTokenService {
     
     @EventListener(ApplicationReadyEvent.class)
     @Scheduled(cron = "0 0 4 * * *")
+    @Transactional
     public void cleanupOldTokens() {
         
         final var cleanUpBefore = LocalDateTime
